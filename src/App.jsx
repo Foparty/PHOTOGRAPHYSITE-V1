2fdcdc4cf65f14ccc6099057b003ec6af3749ce4
Foparty <103907142+Foparty@users.noreply.github.com>
2023-01-18 17:11:32 +0000

    alphav1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
-import { Home, IndividualProject, About, ContactInfo } from './containers/';
+import {
+	Home,
+	IndividualProject,
+	About,
+	ContactInfo,
+	More,
+} from './containers/';
 
 function App() {
 	return (
@@ -11,6 +17,7 @@ function App() {
 				<Route path='/:id' element={<IndividualProject />} />
 				<Route path='/about' element={<About />} />
 				<Route path='/info' element={<ContactInfo />} />
+				<Route path='/more' element={<More />} />
 			</Routes>
 		</>
 	);
--- src/containers/index.js
+++ src/containers/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as IndividualProject } from './IndividualProject';
 export { default as About } from './About';
 export { default as ContactInfo } from './ContactInfo';
+export { default as More } from './More';
